582f3d0a5c7ae6e6687ab97b6291961bec36ca35
Elma Rahman <elmarahman635@gmail.com>
2019-05-19 22:22:42 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,8 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle #bc thats the child within parent
+#so it'll inherit the stuff in vehicle
+  def go
+     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+   end
+ end
